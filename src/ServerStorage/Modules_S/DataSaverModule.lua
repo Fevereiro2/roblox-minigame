bc69f3fc77e3256996e43c4a5d0ea0a1029647b5
Pedro Fevereiro <103332709+Fevereiro2@users.noreply.github.com>
2026-01-05 19:15:36 +0000

    Add player data load and save functions
    Introduced Load_PlayerData and Save_PlayerData functions to DataSaverModule, providing stubs for loading and saving individual player data.

--- src/ServerStorage/Modules_S/DataSaverModule.lua
+++ src/ServerStorage/Modules_S/DataSaverModule.lua
@@ -8,4 +8,12 @@ function DataSaverModule.Load()
 	return {}
 end
 
+function DataSaverModule.Load_PlayerData()
+	return {}
+end
+
+function DataSaverModule.Save_PlayerData()
+	return true
+end
+
 return DataSaverModule
